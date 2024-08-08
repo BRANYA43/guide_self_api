240fb61b2cd2b88e10e7763cde3d0626f1cbd1b7
BRANYA <tbd2504@gmail.com>
2024-08-08 19:16:20 +0300

    add superuser envs

--- src/core/settings/environments.py
+++ src/core/settings/environments.py
@@ -2,13 +2,29 @@
 Environment values
 """
 
-from pydantic import field_validator
+import os
+from typing import Any
+
+from pydantic import field_validator, EmailStr, model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from core.settings import BASE_DIR
 
 
+class SuperuserEnvs(BaseSettings):
+    username: str | None = None
+    email: EmailStr
+    password: str
+
+    @model_validator(mode='before')
+    def set_django_envs(cls, data: dict[str, Any]) -> dict[str, Any]:
+        for name, value in data.items():
+            os.environ.setdefault(f'django_superuser_{name}'.upper(), value)
+        return data
+
+
 class ApiEnvs(BaseSettings):
+    superuser: SuperuserEnvs
     secret_key: str
     debug: bool = False
     allowed_hosts: list[str] | str = ['*']
@@ -23,7 +39,10 @@ class ApiEnvs(BaseSettings):
 class Envs(BaseSettings):
     api: ApiEnvs
 
-    model_config = SettingsConfigDict(env_file=BASE_DIR / '../environments/.env', env_nested_delimiter='__')
+    model_config = SettingsConfigDict(
+        env_file=(BASE_DIR / '../environments/.env'),
+        env_nested_delimiter='__',
+    )
 
 
 envs = Envs()  # type: ignore
