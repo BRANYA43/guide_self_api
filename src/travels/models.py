3d371ebe6bd9bbcddffd22e21d239faaee249741
BRANYA <tbd2504@gmail.com>
2024-08-09 17:53:52 +0300

    create info and migrations

--- src/travels/models.py
+++ src/travels/models.py
@@ -1,7 +1,47 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.core.validators import MinLengthValidator
 from django.db import models
 
 from utils.models import BaseModel
+
+
+class Info(BaseModel):
+    name = models.CharField(
+        verbose_name='Name',
+        max_length=100,
+        validators=[MinLengthValidator(2)],
+    )
+    short_descr = models.CharField(
+        verbose_name='Short Description',
+        max_length=2048,
+        null=True,
+        blank=True,
+    )
+    descr = models.CharField(
+        verbose_name='Full Description',
+        max_length=4096,
+        null=True,
+        blank=True,
+    )
+    lang = models.ForeignKey(
+        verbose_name='Localization',
+        to='Language',
+        on_delete=models.PROTECT,
+    )
+    content_type = models.ForeignKey(
+        to=ContentType,
+        on_delete=models.PROTECT,
+    )
+    object_id = models.UUIDField()
+    content_obj = GenericForeignKey()
+
+    class Meta:
+        verbose_name = ('Localized Information',)
+        verbose_name_plural = 'Localized Information'
+
+    def __str__(self):
+        return str(self.slug)
 
 
 class Language(BaseModel):
--- src/travels/tests/test_models.py
+++ src/travels/tests/test_models.py
@@ -1,5 +1,14 @@
 from django.test import TestCase
-from travels.models import Language
+from travels.models import Language, Info
+
+
+class InfoModelTest(TestCase):
+    def setUp(self):
+        self.lang = Language(slug='ukrainian', code='ua')
+        self.info = Info(slug='info', name='new info', lang=self.lang, content_obj=self.lang)
+
+    def test_model_representation(self):
+        self.assertEqual(str(self.info), self.info.slug)
 
 
 class LanguageModelTest(TestCase):
