ece7dcd6df7adae9cf0d4e820bed135ed0ea7547
BRANYA <tbd2504@gmail.com>
2024-08-09 17:44:26 +0300

    add language to admin site

--- src/travels/admin.py
+++ src/travels/admin.py
@@ -1,1 +1,15 @@
-# Register your models here.
+from django.contrib import admin
+
+from travels.models import Language
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'updated_at', 'created_at')
+    readonly_fields = ('updated_at', 'created_at')
+    fieldsets = (
+        ('General Information', dict(fields=('slug', 'code'))),
+        ('Dates', dict(fields=('updated_at', 'created_at'))),
+    )
+    ordering = ('slug',)
+    search_fields = ('slug', 'code')
