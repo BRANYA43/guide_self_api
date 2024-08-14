9f3feebe58b00b2e3381ac6daa74bcd926ba7bf1
BRANYA <tbd2504@gmail.com>
2024-08-14 16:40:04 +0300

    add info model to admin site

--- src/travels/admin.py
+++ src/travels/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
-from travels.models import Language
+from travels.models import Language, Info
+
+
+@admin.register(Info)
+class InfoAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'lang', 'updated_at', 'created_at')
+    readonly_fields = ('updated_at', 'created_at', 'get_related_obj')
+    fieldsets = (
+        ('General Information', dict(fields=('get_related_obj', 'slug', 'name', 'short_descr', 'descr', 'lang'))),
+        ('Dates', dict(fields=('updated_at', 'created_at'))),
+    )
+    ordering = ('lang', 'slug')
+    search_fields = ('slug', 'lang__slug', 'lang__code')
+
+    @admin.display(description='Related Object')
+    def get_related_obj(self, instance: Info):
+        return str(instance.content_obj)
+
+    def has_add_permission(self, request):
+        return False
 
 
 @admin.register(Language)
