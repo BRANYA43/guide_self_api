a83c928c05c60d39dcf9b53037b6e1526dbbefa2
BRANYA <tbd2504@gmail.com>
2024-08-14 16:42:42 +0300

    add country model to admin site

--- src/travels/admin.py
+++ src/travels/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline, GenericStackedInline
 
-from travels.models import Language, Info, MainImage, ExtraImage
+from travels.models import Language, Info, MainImage, ExtraImage, Country
 
 
 class MainImageInline(GenericStackedInline):
@@ -32,6 +32,19 @@ class InfoInline(GenericTabularInline):
     readonly_fields = ('updated_at', 'created_at')
     extra = 1
     show_change_link = True
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'updated_at', 'created_at')
+    readonly_fields = ('updated_at', 'created_at')
+    fieldsets = (
+        ('General Information', dict(fields=('slug',))),
+        ('Dates', dict(fields=('updated_at', 'created_at'))),
+    )
+    ordering = ('slug',)
+    search_fields = ('slug',)
+    inlines = (MainImageInline, ExtraImageInline, InfoInline)
 
 
 @admin.register(Info)
