e2928d522143ba385851f9c6b9aea61e0d042c01
BRANYA <tbd2504@gmail.com>
2024-08-19 15:24:37 +0300

    create api and add its urls to core urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -7,8 +7,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from core.api import api
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
