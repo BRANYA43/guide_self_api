13a3482ec34e61ea5fc2949ec2231a1389726a5f
BRANYA <tbd2504@gmail.com>
2024-08-20 19:30:16 +0300

    add baton urls to core urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,12 +5,13 @@ URL configuration for core project.
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('baton/', include('baton.urls')),
     path('api/', api.urls),
 ]
 
