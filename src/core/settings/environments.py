040de821d8cf5e2cf92fe7944a81be47714d095b
BRANYA <tbd2504@gmail.com>
2024-08-23 19:05:13 +0300

    refactor envs to fix errors

--- src/core/settings/environments.py
+++ src/core/settings/environments.py
@@ -2,32 +2,25 @@
 Environment values
 """
 
-import os
-from typing import Any
+from typing import Annotated
 
-from pydantic import field_validator, EmailStr, model_validator
+from pydantic import field_validator, EmailStr, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from core.settings import BASE_DIR
 
 
-class SuperuserEnvs(BaseSettings):
-    username: str | None = None
-    email: EmailStr
-    password: str
-
-    @model_validator(mode='before')
-    def set_django_envs(cls, data: dict[str, Any]) -> dict[str, Any]:
-        for name, value in data.items():
-            os.environ.setdefault(f'django_superuser_{name}'.upper(), value)
-        return data
+class _BaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(BASE_DIR / '../environments/.env'),
+        extra='allow',
+    )
 
 
-class ApiEnvs(BaseSettings):
-    superuser: SuperuserEnvs
-    secret_key: str
-    debug: bool = False
-    allowed_hosts: list[str] | str = ['*']
+class ApiEnvs(_BaseSettings):
+    secret_key: Annotated[str, Field(alias='DJANGO_SECRET_KEY')]
+    debug: Annotated[bool, Field(False, alias='DJANGO_DEBUG')]
+    allowed_hosts: Annotated[list[str], str, Field(['*'], alias='DJANGO_ALLOWED_HOST')]
 
     @field_validator('allowed_hosts', mode='before')
     def split_allowed_hosts(cls, v: str | list[str]) -> list[str]:
@@ -36,13 +29,15 @@ class ApiEnvs(BaseSettings):
         return v
 
 
-class Envs(BaseSettings):
-    api: ApiEnvs
-
-    model_config = SettingsConfigDict(
-        env_file=(BASE_DIR / '../environments/.env'),
-        env_nested_delimiter='__',
-    )
+class SuperuserEnvs(_BaseSettings):
+    username: Annotated[str, None, Field(None, alias='DJANGO_SUPERUSER_USERNAME')]
+    email: Annotated[EmailStr, Field(alias='DJANGO_SUPERUSER_EMAIL')]
+    password: Annotated[str, Field(alias='DJANGO_SUPERUSER_EMAIL')]
 
 
-envs = Envs()  # type: ignore
+class Envs:
+    api = ApiEnvs()  # type: ignore
+    superuser = SuperuserEnvs()  # type: ignore
+
+
+envs = Envs
