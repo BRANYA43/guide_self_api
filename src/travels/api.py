17747a2067b5b0d1abfa0f2f9654942d337e334f
BRANYA <tbd2504@gmail.com>
2024-08-24 15:41:10 +0300

    create get country detail api

--- src/travels/api.py
+++ src/travels/api.py
@@ -1,14 +1,17 @@
 from typing import Annotated
 
 from django.db.models import Prefetch
-from ninja import Router, Header
+from django.shortcuts import get_object_or_404
+from ninja import Router, Header, Path
+from pydantic import UUID4
 
 from travels.models import Country, Info
-from travels.schemas import CountrySummarySchema
+from travels.schemas import CountrySummarySchema, CountryDetailSchema
 
 router = Router(tags=['travels'])
 
 AcceptLanguage = Annotated[str, Header(alias='Accept-Language')]
+ID = Annotated[UUID4, Path()]
 
 
 @router.get('/countries', response=list[CountrySummarySchema])
@@ -18,3 +21,14 @@ def get_country_list(request, lang: AcceptLanguage):
         'main_image',
     )
     return countries
+
+
+@router.get('/countries/{id}', response=CountryDetailSchema)
+def get_country_detail(request, id: ID, lang: AcceptLanguage):
+    return get_object_or_404(
+        Country.objects.prefetch_related(
+            Prefetch('info', Info.objects.filter(lang__code=lang)),
+            'main_image',
+        ),
+        id=id,
+    )
--- src/travels/tests/test_api.py
+++ src/travels/tests/test_api.py
@@ -1,10 +1,45 @@
+from uuid import uuid4
+
 from django.core.files.uploadedfile import SimpleUploadedFile
 from ninja.testing import TestClient
 
 from travels.api import router
 from travels.models import Country
-from travels.schemas import CountrySummarySchema
+from travels.schemas import CountrySummarySchema, CountryDetailSchema
 from utils.tests import MediaTestCase
+
+
+class CountryRetrieveAPITest(MediaTestCase):
+    def setUp(self):
+        self.schema = CountryDetailSchema
+        self.client = TestClient(router)
+        self.country = self.create_test_country(slug='ukraine')
+        self.lang = self.create_test_language(slug='english', code='en')
+        self.file = SimpleUploadedFile('main_image.png', b'image', 'image/png')
+        self.create_test_info(
+            self.country,
+            lang=self.lang,
+            slug=self.country.slug + '_' + self.lang.code,
+            name=self.country.slug.title(),
+            full_descr='Full Lorem Text',
+        )
+        self.create_test_image(
+            self.country,
+            file=self.file,
+            slug=self.country.slug + '_main_image',
+        )
+
+    def test_api_returns_country_detail_data(self):
+        expected_data = dict(**self.schema.from_orm(self.country).dict(exclude=['id']), id=str(self.country.id))
+        response = self.client.get(f'/countries/{self.country.id}', headers={'Accept-Language': 'en'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), expected_data)
+
+    def test_api_returns_404_error(self):
+        response = self.client.get(f'/countries/{uuid4()}', headers={'Accept-Language': 'en'})
+
+        self.assertEqual(response.status_code, 404)
 
 
 class CountryListAPITest(MediaTestCase):
