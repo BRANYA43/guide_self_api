6b001ecf27aee205737c90d4b8b2baa400e7f6ff
BRANYA <tbd2504@gmail.com>
2024-08-24 21:57:56 +0300

    update packages

--- src/core/settings/database.py
+++ src/core/settings/database.py
@@ -11,7 +11,7 @@ SQLITE3 = {
 }
 
 POSTGRESQL = {
-    'ENGINE': 'django.db.backends.postgres',
+    'ENGINE': 'django.db.backends.postgresql',
     'NAME': envs.db.name,
     'HOST': envs.db.host,
     'PORT': envs.db.port,
